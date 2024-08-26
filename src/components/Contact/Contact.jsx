9d116c2eac13ddb16c3643804039c307c789e3f7
chethan7032 <chethanreddy118@gmail.com>
2024-08-26 12:22:36 +0530

    first commit

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+import styles from "./Contact.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Contact = () => {
+  return (
+    <footer id="contact" className={styles.container}>
+      <div className={styles.text}>
+        <h2>Contact</h2>
+        <p>Feel free to reach out!</p>
+      </div>
+      <ul className={styles.links}>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
+          <a href="mailto:chethanreddy118@gmail.com">
+            chethanreddy118@gmail.com
+          </a>
+        </li>
+        <li className={styles.link}>
+          <img
+            src={getImageUrl("contact/linkedinIcon.png")}
+            alt="LinkedIn icon"
+          />
+          <a href="https://www.linkedin.com/in/g-chethan-reddy-33924b25a/">
+            linkedin.com/chethan
+          </a>
+        </li>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
+          <a href="https://github.com/chethan7032">github.com/chethan</a>
+        </li>
+      </ul>
+    </footer>
+  );
+};
